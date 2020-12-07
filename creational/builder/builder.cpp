ac4a948cbe812a576cbc34f8711cec06e580ab5c
docbrown85 <me@example.com>
2020-12-07 18:10:09 +0100

    Add Builder pattern scaffolding

---
+++ creational/builder/builder.cpp
@@ -0,0 +1,20 @@
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <utility>
+
+namespace creational
+{
+    namespace builder
+    {
+        // TODO
+    } // namespace builder
+
+} // namespace creational
+
+int main()
+{
+    // TODO
+
+    return 0;
+}
