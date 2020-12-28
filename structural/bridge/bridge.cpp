0646eb1f6aaad2c1b70795fb6a4c68092c0d6fc9
docbrown85 <me@example.com>
2020-12-28 12:46:25 +0100

    Add Bridge Pattern implementation

--- structural/bridge/bridge.cpp
+++ structural/bridge/bridge.cpp
@@ -7,6 +7,78 @@ namespace structural
 {
     namespace bridge
     {
+        class Implementor
+        {
+        public:
+            virtual ~Implementor() {}
+            virtual std::string operation_implementation() const = 0;
+        };
+
+        class ConcreteImplementorA : public Implementor
+        {
+        public:
+            std::string operation_implementation() const override
+            {
+                return "ConcreteImplementorA operation implementation";
+            }
+        };
+
+        class ConcreteImplementorB : public Implementor
+        {
+        public:
+            std::string operation_implementation() const override
+            {
+                return "ConcreteImplementorB operation implementation";
+            }
+        };
+
+        class Abstraction
+        {
+        protected:
+            std::unique_ptr<Implementor> _implementor;
+
+        public:
+            Abstraction(std::unique_ptr<Implementor> implementor) : _implementor(std::move(implementor))
+            {
+            }
+
+            virtual ~Abstraction() {}
+
+            virtual std::string operation() const
+            {
+                return "Abstraction basic implementation using: " + _implementor->operation_implementation();
+            }
+        };
+
+        class RefinedAbstraction : public Abstraction
+        {
+        public:
+            RefinedAbstraction(std::unique_ptr<Implementor> implementor) : Abstraction(std::move(implementor)) {}
+
+            std::string operation() const override
+            {
+                return "RefinedAbstraction implementation using: " + _implementor->operation_implementation();
+            }
+        };
+
+        class Client
+        {
+        public:
+            Client(std::unique_ptr<Abstraction> abstraction) : _abstraction(std::move(abstraction)) {}
+
+            void set_abstraction(std::unique_ptr<Abstraction> abstraction)
+            {
+                _abstraction = std::move(abstraction);
+            }
+
+            void execute()
+            {
+                std::cout << _abstraction->operation() << "\n";
+            }
+
+        private:
+            std::unique_ptr<Abstraction> _abstraction;
+        };
 
     } // namespace bridge
 
@@ -14,5 +86,15 @@ namespace structural
 
 int main()
 {
+    std::unique_ptr<structural::bridge::Implementor> concrete_implementor_a = std::make_unique<structural::bridge::ConcreteImplementorA>();
+    std::unique_ptr<structural::bridge::Abstraction> abstraction = std::make_unique<structural::bridge::Abstraction>(std::move(concrete_implementor_a));
+    structural::bridge::Client client(std::move(abstraction));
+    client.execute();
+
+    std::unique_ptr<structural::bridge::Implementor> concrete_implementor_b = std::make_unique<structural::bridge::ConcreteImplementorB>();
+    std::unique_ptr<structural::bridge::RefinedAbstraction> refined_abstraction = std::make_unique<structural::bridge::RefinedAbstraction>(std::move(concrete_implementor_b));
+    structural::bridge::Client refined_client(std::move(refined_abstraction));
+    refined_client.execute();
+
     return 0;
 }
