f350f7daec94560349e873c31b4911057dc37508
docbrown85 <me@example.com>
2021-01-16 14:17:19 +0100

    Add FlyweightFactory (WIP)

--- structural/flyweight/flyweight.cpp
+++ structural/flyweight/flyweight.cpp
@@ -1,6 +1,7 @@
 #include <cstring>
 #include <iostream>
 #include <memory>
+#include <unordered_map>
 
 namespace structural
 {
@@ -71,6 +72,49 @@ namespace structural
 
         private:
             std::shared_ptr<IntrinsicState> _intrinsic_state;
+        };
+
+        /**
+         * The Flyweight Factory creates and manages the Flyweight objects. It ensures
+         * that flyweights are shared correctly. When the client requests a flyweight,
+         * the factory either returns an existing instance or creates a new one, if it
+         * doesn't exist yet.
+         */
+        class FlyweightFactory
+        {
+        public:
+            FlyweightFactory(std::initializer_list<IntrinsicState> intrinsic_states)
+            {
+                for (const IntrinsicState &is : intrinsic_states)
+                {
+                    const std::shared_ptr<IntrinsicState> intrinsic_state = std::make_shared<IntrinsicState>(is);
+                    this->_flyweights.insert(
+                        std::make_pair<std::string, std::shared_ptr<Flyweight>>(
+                            this->get_key(is), std::make_shared<Flyweight>(intrinsic_state)));
+                }
+            }
+
+            /**
+             * Returns an existing Flyweight with a given state or creates a new one.
+             */
+
+        private:
+            std::unordered_map<std::string, std::shared_ptr<Flyweight>> _flyweights;
+
+            std::string get_key(const std::shared_ptr<IntrinsicState> &intrinsic_state) const
+            {
+                return get_key(intrinsic_state->_attribute_a, intrinsic_state->_attribute_b, intrinsic_state->_attribute_c);
+            }
+
+            std::string get_key(const IntrinsicState &intrinsic_state) const
+            {
+                return get_key(intrinsic_state._attribute_a, intrinsic_state._attribute_b, intrinsic_state._attribute_c);
+            }
+
+            std::string get_key(const std::string a, const std::string b, const std::string c) const
+            {
+                return a + "_" + b + "_" + c;
+            }
         };
 
     } // namespace flyweight
