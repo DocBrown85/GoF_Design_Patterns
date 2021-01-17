1caa635f7c12bae60cd6c49a6a6abc7da5446e16
docbrown85 <me@example.com>
2021-01-17 19:23:01 +0100

    Complete Flyweight Pattern code

--- structural/flyweight/flyweight.cpp
+++ structural/flyweight/flyweight.cpp
@@ -97,6 +97,34 @@ namespace structural
             /**
              * Returns an existing Flyweight with a given state or creates a new one.
              */
+            std::shared_ptr<Flyweight> get_flyweight(const IntrinsicState &is)
+            {
+                std::string key = get_key(is);
+                if (this->_flyweights.find(key) == this->_flyweights.end())
+                {
+                    std::cout << "FlyweightFactory: Can't find a flyweight, creating new one.\n";
+                    const std::shared_ptr<IntrinsicState> intrinsic_state = std::make_shared<IntrinsicState>(is);
+                    this->_flyweights.insert(
+                        std::make_pair<std::string, std::shared_ptr<Flyweight>>(
+                            this->get_key(is), std::make_shared<Flyweight>(intrinsic_state)));
+                }
+                else
+                {
+                    std::cout << "FlyweightFactory: Reusing existing flyweight.\n";
+                }
+                return this->_flyweights.at(key);
+            }
+
+            void list_flyweights() const
+            {
+                size_t count = this->_flyweights.size();
+                std::cout << "\nFlyweightFactory: I have " << count << " flyweights:\n";
+                for (std::pair<std::string, std::shared_ptr<Flyweight>> pair : this->_flyweights)
+                {
+                    std::cout << pair.first << "\n";
+                }
+                std::cout << "\n";
+            }
 
         private:
             std::unordered_map<std::string, std::shared_ptr<Flyweight>> _flyweights;
@@ -117,11 +145,43 @@ namespace structural
             }
         };
 
+        class Client
+        {
+        public:
+            Client(std::unique_ptr<FlyweightFactory> flyweight_factory) : _flyweight_factory(std::move(flyweight_factory)) {}
+
+            void execute() const
+            {
+                _flyweight_factory->list_flyweights();
+
+                std::shared_ptr<Flyweight> flyweight = nullptr;
+
+                // This call will reuse an existing flyweight
+                flyweight = _flyweight_factory->get_flyweight({"A1", "B1", "C1"});
+                flyweight->operation({"D1", "E1"});
+
+                // This call will create a new flyweight
+                flyweight = _flyweight_factory->get_flyweight({"A3", "B3", "C3"});
+                flyweight->operation({"D3", "E3"});
+
+                _flyweight_factory->list_flyweights();
+            }
+
+        private:
+            std::unique_ptr<FlyweightFactory> _flyweight_factory;
+        };
+
     } // namespace flyweight
 
 } // namespace structural
 
 int main()
 {
+    std::unique_ptr<structural::flyweight::FlyweightFactory> flyweight_factory = std::make_unique<structural::flyweight::FlyweightFactory>(
+        std::initializer_list<structural::flyweight::IntrinsicState>{
+            {"A0", "B0", "C0"}, {"A1", "B1", "C1"}, {"A2", "B2", "C2"}});
+
+    structural::flyweight::Client client(std::move(flyweight_factory));
+    client.execute();
     return 0;
 }
