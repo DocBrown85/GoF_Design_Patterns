ae026829c327fecd7dd1670b1b603a11de87a027
docbrown85 <me@example.com>
2021-02-27 12:02:11 +0100

    Add smart pointers to Mediator

--- behavioral/mediator/mediator.cpp
+++ behavioral/mediator/mediator.cpp
@@ -12,67 +12,72 @@ namespace behavioral
         class Mediator
         {
         public:
-            virtual void notify(Colleague *sender, std::string event) const = 0;
+            virtual void notify(std::shared_ptr<Colleague> sender, std::string event) const = 0;
         };
 
         class Colleague
         {
         public:
-            Colleague(Mediator *mediator = nullptr) : _mediator(mediator)
+            Colleague(std::shared_ptr<Mediator> mediator = nullptr) : _mediator(mediator)
             {
             }
-            void set_mediator(Mediator *mediator)
+            void set_mediator(std::shared_ptr<Mediator> mediator)
             {
                 this->_mediator = mediator;
             }
 
         protected:
-            Mediator *_mediator;
+            std::shared_ptr<Mediator> _mediator;
         };
 
-        class Colleague1 : public Colleague
+        class Colleague1 : public Colleague, public std::enable_shared_from_this<Colleague>
         {
         public:
             void do_a()
             {
                 std::cout << "Colleague1.do_a()\n";
-                this->_mediator->notify(this, "A");
+                this->_mediator->notify(shared_from_this(), "A");
             }
             void do_b()
             {
                 std::cout << "Colleague1.do_b()\n";
-                this->_mediator->notify(this, "B");
+                this->_mediator->notify(shared_from_this(), "B");
             }
         };
 
-        class Colleague2 : public Colleague
+        class Colleague2 : public Colleague, public std::enable_shared_from_this<Colleague>
         {
         public:
             void do_c()
             {
                 std::cout << "Colleague2.do_c()\n";
-                this->_mediator->notify(this, "C");
+                this->_mediator->notify(shared_from_this(), "C");
             }
             void do_d()
             {
                 std::cout << "Colleague2.do_d()\n";
-                this->_mediator->notify(this, "D");
+                this->_mediator->notify(shared_from_this(), "D");
             }
         };
 
-        class ConcreteMediator : public Mediator
+        class ConcreteMediator : public Mediator, public std::enable_shared_from_this<Mediator>
         {
-        private:
-            Colleague1 *_colleague1;
-            Colleague2 *_colleague2;
-
         public:
-            ConcreteMediator(Colleague1 *colleague1, Colleague2 *colleague2) : _colleague1(colleague1), _colleague2(colleague2)
+            ConcreteMediator(std::shared_ptr<Colleague1> colleague1, std::shared_ptr<Colleague2> colleague2) : _colleague1(colleague1), _colleague2(colleague2)
             {
-                this->_colleague1->set_mediator(this);
-                this->_colleague2->set_mediator(this);
             }
-            void notify(Colleague *sender, std::string event) const override
+
+            /*
+             * This is needed because we cannot use shared_from_this() in constructors since there is no valid shared pointer
+             * available while executing constructors.
+             */
+            void setup_collegues()
+            {
+                this->_colleague1->set_mediator(shared_from_this());
+                this->_colleague2->set_mediator(shared_from_this());
+            }
+
+            void notify(std::shared_ptr<Colleague> sender, std::string event) const override
             {
                 if (event == "A")
                 {
@@ -86,6 +91,10 @@ namespace behavioral
                     this->_colleague2->do_c();
                 }
             }
+
+        private:
+            std::shared_ptr<Colleague1> _colleague1;
+            std::shared_ptr<Colleague2> _colleague2;
         };
 
         class Client
@@ -93,18 +102,15 @@ namespace behavioral
         public:
             void execute()
             {
-                Colleague1 *colleague1 = new Colleague1;
-                Colleague2 *colleague2 = new Colleague2;
-                ConcreteMediator *mediator = new ConcreteMediator(colleague1, colleague2);
+                std::shared_ptr<Colleague1> colleague1 = std::make_shared<Colleague1>();
+                std::shared_ptr<Colleague2> colleague2 = std::make_shared<Colleague2>();
+                std::shared_ptr<ConcreteMediator> mediator = std::make_shared<ConcreteMediator>(colleague1, colleague2);
+                mediator->setup_collegues();
                 std::cout << "Client triggers operation A.\n";
                 colleague1->do_a();
                 std::cout << "\n";
                 std::cout << "Client triggers operation D.\n";
                 colleague2->do_d();
-
-                delete colleague1;
-                delete colleague2;
-                delete mediator;
             }
         };
 
