135cde94a2fc0bdd1ad61f398220e0e5ce6e5e2d
docbrown85 <me@example.com>
2021-03-13 13:17:48 +0100

    Add Template Method implementation

--- behavioral/template_method/template_method.cpp
+++ behavioral/template_method/template_method.cpp
@@ -9,11 +9,81 @@ namespace behavioral
 {
     namespace template_method
     {
+        class AbstractClass
+        {
+        public:
+            // the skeleton of an algorithm
+            void template_method() const
+            {
+                this->base_operation_1();
+                this->required_operation_1();
+                this->base_operation_2();
+                this->hook_1();
+                this->required_operation_2();
+                this->base_operation_3();
+                this->hook_2();
+            }
+
+        protected:
+            // parts of the algorithm implemented by base class, but overridable if needed
+            void base_operation_1() const
+            {
+                std::cout << "AbstractClass: base_operation_1\n";
+            }
+            void base_operation_2() const
+            {
+                std::cout << "AbstractClass: base_operation_2\n";
+            }
+            void base_operation_3() const
+            {
+                std::cout << "AbstractClass: base_operation_3\n";
+            }
+
+            // steps of the algoritm that have to be implemented by subclasses
+            virtual void required_operation_1() const = 0;
+            virtual void required_operation_2() const = 0;
+
+            // hooks overridable by subclasses if needed, not mandatory
+            virtual void hook_1() const {}
+            virtual void hook_2() const {}
+        };
+
+        class ConcreteClass1 : public AbstractClass
+        {
+        protected:
+            void required_operation_1() const override
+            {
+                std::cout << "ConcreteClass1: required_operation_1\n";
+            }
+            void required_operation_2() const override
+            {
+                std::cout << "ConcreteClass1: required_operation_2\n";
+            }
+        };
+
+        class ConcreteClass2 : public AbstractClass
+        {
+        protected:
+            void required_operation_1() const override
+            {
+                std::cout << "ConcreteClass2: required_operation_1\n";
+            }
+            void required_operation_2() const override
+            {
+                std::cout << "ConcreteClass2: required_operation_2\n";
+            }
+            void hook_1() const override
+            {
+                std::cout << "ConcreteClass2: hook_1\n";
+            }
+        };
+
         class Client
         {
         public:
-            void execute()
+            void execute(AbstractClass *abstract_class)
             {
+                abstract_class->template_method();
             }
         };
 
@@ -25,6 +95,14 @@ int main()
 {
     behavioral::template_method::Client client;
 
-    client.execute();
+    behavioral::template_method::ConcreteClass1 *concrete_class_1 = new behavioral::template_method::ConcreteClass1();
+    client.execute(concrete_class_1);
+
+    behavioral::template_method::ConcreteClass2 *concrete_class_2 = new behavioral::template_method::ConcreteClass2();
+    client.execute(concrete_class_2);
+
+    delete concrete_class_1;
+    delete concrete_class_2;
+
     return 0;
 }
